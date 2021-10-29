98d1bd6f680022aaddcfb3860179ea322b11b941
Sergio Martins <sergiomos2010@gmail.com>
2021-10-29 16:21:18 -0300

    add & config jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  clearMocks: true,
+  preset: '',
+  projects: ['<rootDir>/packages/**/jest.config.js'],
+  testEnvironment: 'node',
+  testMatch: ['*.test.js', '*.test.jsx'],
+};
