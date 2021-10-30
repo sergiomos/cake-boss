e07089e3544925b5b3ac23296912516127f66e26
Sergio Martins <sergiomos2010@gmail.com>
2021-10-29 21:33:04 -0300

    test: insert manager request fields validation

--- packages/server/src/models/Manager.model.js
+++ packages/server/src/models/Manager.model.js
@@ -3,8 +3,15 @@ const conn = require('../database/connection');
 const COLLECTION_NAME = 'managers';
 
 exports.create = async ({ email, name, password }) => {
-  const managerCollection = await conn().db.collection(COLLECTION_NAME);
-  const createdManager = await managerCollection.insertOne({ email, name, password });
+  const db = await conn();
+  const managerCollection = await db.collection(COLLECTION_NAME);
+  const { insertedId } = await managerCollection.insertOne({ email, name, password });
+
+  const createdManager = {
+    email,
+    name,
+    _id: insertedId,
+  };
 
   return createdManager;
 };
