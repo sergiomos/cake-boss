3963157ae493e51083e331731a75127e073b5c23
Sergio Martins <sergiomos2010@gmail.com>
2021-10-31 10:49:28 -0300

    test: insert raw material fail

--- packages/server/__tests__/integration/rawMateirals.test.js
+++ packages/server/__tests__/integration/rawMateirals.test.js
@@ -35,8 +35,8 @@ describe('POST /rawMaterials', () => {
     });
 
     describe('on success', () => {
-      it('return material name', () => {
-        const response = request(app)
+      it('return material name', async () => {
+        const response = await request(app)
           .post(API_ROUTE)
           .send({
             name: 'Farinha de Trigo',
@@ -48,8 +48,8 @@ describe('POST /rawMaterials', () => {
         expect(response.body.name).toBe('Farinha de Trigo');
       });
 
-      it('return userId from who insert', () => {
-        const response = request(app)
+      it('return userId from who insert', async () => {
+        const response = await request(app)
           .post(API_ROUTE)
           .send({
             name: 'Farinha de Trigo',
@@ -61,8 +61,8 @@ describe('POST /rawMaterials', () => {
         expect(response.body.userId).toStrictEqual(userId);
       });
 
-      it('return the quantity', () => {
-        const response = request(app)
+      it('return the quantity', async () => {
+        const response = await request(app)
           .post(API_ROUTE)
           .send({
             name: 'Farinha de Trigo',
@@ -72,6 +72,134 @@ describe('POST /rawMaterials', () => {
 
         expect(response.statusCode).toBe(200);
         expect(response.body.quantity).toBe(10);
+      });
+    });
+
+    describe('on fail', () => {
+      describe('return error messages by case', () => {
+        describe("Can't insert with invalid", () => {
+          describe('name', () => {
+            it('should not be empty', async () => {
+              const response = await request(app)
+                .post(API_ROUTE)
+                .send({
+                  quantity: 10,
+                  userId,
+                });
+
+              expect(response.statusCode).toBe(400);
+              expect(response.body).toStrictEqual({
+                err: {
+                  message: "name can't be empty",
+                },
+              });
+            });
+
+            it('should be equal or larger than 3', async () => {
+              const response = await request(app)
+                .post(API_ROUTE)
+                .send({
+                  name: 'Ov',
+                  quantity: 5,
+                  userId,
+                });
+
+              expect(response.statusCode).toBe(400);
+              expect(response.body).toStrictEqual({
+                err: {
+                  message: 'name must be equal or larger than 3',
+                },
+              });
+            });
+          });
+
+          describe('quantity', () => {
+            it('should not be empty', async () => {
+              const response = await request(app)
+                .post(API_ROUTE)
+                .send({
+                  name: 'Farinha de Trigo',
+                  userId,
+                });
+
+              expect(response.statusCode).toBe(400);
+              expect(response.body).toStrictEqual({
+                err: {
+                  message: "quantity can't be empty",
+                },
+              });
+            });
+
+            it('Should be greater than 0', async () => {
+              const response = await request(app)
+                .post(API_ROUTE)
+                .send({
+                  name: 'Ov',
+                  quantity: -2,
+                  userId,
+                });
+
+              expect(response.statusCode).toBe(400);
+              expect(response.body).toStrictEqual({
+                err: {
+                  message: 'quantity must be greater than 0',
+                },
+              });
+            });
+          });
+        });
+
+        describe('userId', () => {
+          it('should not be empty', async () => {
+            const response = await request(app)
+              .post(API_ROUTE)
+              .send({
+                name: 'Farinha de Trigo',
+                quantity: 10,
+              });
+
+            expect(response.statusCode).toBe(400);
+            expect(response.body).toStrictEqual({
+              err: {
+                message: "userId can't be empty",
+              },
+            });
+          });
+
+          it('should be valid', async () => {
+            const response = await request(app)
+              .post(API_ROUTE)
+              .send({
+                name: 'Ov',
+                quantity: 5,
+                userId: '516512',
+              });
+
+            expect(response.statusCode).toBe(400);
+            expect(response.body).toStrictEqual({
+              err: {
+                message: 'userId invalid',
+              },
+            });
+          });
+
+          it('should exists', async () => {
+            const response = await request(app)
+              .post(API_ROUTE)
+              .send({
+                name: 'Ov',
+                quantity: 5,
+                userId: '617deb2d3571e4ee657a88b3',
+              });
+
+            expect(response.statusCode).toBe(400);
+            expect(response.body).toStrictEqual({
+              err: {
+                message: 'user not found',
+              },
+            });
+          });
+        });
       });
     });
   });
