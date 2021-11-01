a7474def85987974aeb050d925f38ad34e13a1cd
Sergio Martins <sergiomos2010@gmail.com>
2021-11-01 19:21:26 -0300

    feat: user sing in validations & messages

--- packages/web/src/components/LoginForm/index.jsx
+++ packages/web/src/components/LoginForm/index.jsx
@@ -1,34 +1,62 @@
-import React, { useContext } from 'react';
-import { Container, Form } from './style';
+import React from 'react';
+import { func } from 'prop-types';
+import useUserContext from '../../hooks/useUserContext';
 
 import TextInput from '../TextInput';
 import SubmitButton from '../SubmitButton';
 
-const LoginForm = () => {
+import { Container, Form } from './style';
+
+const LoginForm = ({ handleUserLogin }) => {
   const {
-    email, password, setEmail, setPassword,
-  } = useContext();
+    email,
+    password,
+    setEmail,
+    setPassword,
+    loginStatus,
+  } = useUserContext();
+
+  const handleInputChange = ({ target }, callback) => {
+    callback(target.value);
+  };
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={(e) => {
+        e.preventDefault();
+        handleUserLogin(email, password);
+      }}
+      >
         <TextInput
-          type="text"
+          type="email"
           placeholder="Email"
+          value={email}
+          onChange={(e) => handleInputChange(e, setEmail)}
+          required
         />
         <TextInput
           type="password"
           placeholder="Senha"
+          value={password}
+          onChange={(e) => handleInputChange(e, setPassword)}
+          required
         />
 
         <SubmitButton
           type="submit"
           bgColor="#1BA29D"
+          disabled={loginStatus === 'loading'}
         >
-          Entrar
+          {loginStatus === 'loading' ? 'Carregando' : 'Entrar'}
         </SubmitButton>
+
       </Form>
     </Container>
   );
 };
+
+LoginForm.propTypes = {
+  handleUserLogin: func,
+}.isRequired;
 
 export default LoginForm;
--- packages/web/src/pages/login/index.jsx
+++ packages/web/src/pages/login/index.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import useUserContext from '../../hooks/useUserContext';
 import LoginForm from '../../components/LoginForm';
 
-const Login = () => (
-  <div>
-    <LoginForm />
-  </div>
-);
+const Login = () => {
+  const { singIn, loginStatus, setLoginStatus } = useUserContext();
+  const [loginStatusMessage, setLoginStatusMessage] = useState('');
+
+  const handleUserLogin = async (userEmail, userPassword) => {
+    setLoginStatus('loading');
+    await singIn(userEmail, userPassword);
+
+    switch (loginStatus) {
+      case 401:
+        setLoginStatusMessage('Email ou senha inválidos, verifique as informações e tente novamente');
+        break;
+      case 200:
+        setLoginStatusMessage('Login realizado com sucesso');
+        break;
+      default:
+    }
+  };
+
+  useEffect(() => {
+    const time = 5000;
+    setTimeout(() => {
+      setLoginStatusMessage('');
+    }, time);
+  }, [loginStatusMessage]);
+
+  return (
+    <div>
+      {loginStatusMessage}
+      <LoginForm
+        handleUserLogin={handleUserLogin}
+      />
+    </div>
+  );
+};
 
 export default Login;
