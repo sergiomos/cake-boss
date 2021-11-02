57283cf1f9c145e22bb3232318103e03307df279
Sergio Martins <sergiomos2010@gmail.com>
2021-11-02 03:55:50 -0300

    feat: create get employees orders

--- packages/web/src/pages/Manager/index.jsx
+++ packages/web/src/pages/Manager/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 
 import useUserContext from '../../hooks/useUserContext';
+import getEmployeeOrders from '../../services/getEmployeeOrders';
 
 import {
   Container, SearchBtn, SearchBar, FlexBox,
@@ -9,6 +10,7 @@ import {
 
 const ManagerHome = () => {
   const { user } = useUserContext();
+  const [employeeName, setEmployeeName] = useState('');
 
   if (user.role !== 'manager') return (<Redirect to="/" />);
   return (
@@ -17,11 +19,14 @@ const ManagerHome = () => {
         <SearchBar
           type="text"
           placeholder="Digite o nome de um colaborador"
+          value={employeeName}
+          onChange={({ target }) => setEmployeeName(target.value)}
         />
 
         <SearchBtn
           type="button"
           bgColor="#33CA7F"
+          onClick={getEmployeeOrders}
         >
           Pesquisar
         </SearchBtn>
