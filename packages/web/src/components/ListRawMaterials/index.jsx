8eff1eb812949c823720b9873a47d3828f35ae55
Sergio Martins <sergiomos2010@gmail.com>
2021-11-04 15:44:28 -0300

    feat: create handle order request

--- packages/web/src/components/ListRawMaterials/index.jsx
+++ packages/web/src/components/ListRawMaterials/index.jsx
@@ -5,33 +5,50 @@ import { Table, Th, Td } from '../Table';
 
 import { RequestButton, RequestTextInput } from './style';
 
-const ListRawMaterials = ({ rawMaterials }) => (
-  <Table>
-    <thead>
-      <tr>
-        <Th>Nome</Th>
-        <Th>Em estoque</Th>
-        <Th>Fazer pedido</Th>
-      </tr>
-    </thead>
+const ListRawMaterials = ({ rawMaterials }) => {
+  const handleOrderRequest = (e, rawMaterialId) => {
+    const { value: quantity } = e.target.previousSibling;
+    console.log(rawMaterialId);
+    console.log(quantity);
+  };
 
-    <tbody>
-      { rawMaterials.map((rawMaterial) => (
+  return (
+    <Table>
+      <thead>
         <tr>
-          <Td>{rawMaterial.name}</Td>
-          <Td>{rawMaterial.quantity}</Td>
-          <Td>
-            <RequestTextInput type="number" placeholder="Qt." />
-            <RequestButton type="Button">Retirar</RequestButton>
-          </Td>
+          <Th>Nome</Th>
+          <Th>Em estoque</Th>
+          <Th>Fazer pedido</Th>
         </tr>
-      ))}
-    </tbody>
-  </Table>
-);
+      </thead>
+
+      <tbody>
+        { rawMaterials.map((rawMaterial) => (
+          <tr>
+            <Td>{rawMaterial.name}</Td>
+            <Td>{rawMaterial.quantity}</Td>
+            <Td>
+              <RequestTextInput
+                type="number"
+                placeholder="Qt."
+              />
+              <RequestButton
+                type="Button"
+                onClick={(e) => handleOrderRequest(e, rawMaterial._id)}
+              >
+                Retirar
+
+              </RequestButton>
+            </Td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
 
 ListRawMaterials.propTypes = {
-  orders: arrayOf(object),
+  rawMaterials: arrayOf(object),
 }.isRequired;
 
 export default ListRawMaterials;
